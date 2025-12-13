5a1d4afb6ff20d2a3a54f5affea3f5f3048c1c34
NANDANI SRIVASTAVA <srivastavanandani190@gmail.com>
2025-12-13 12:16:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5379422803-3d01c",
+  "appId": "1:453370954090:web:c842199cb3046d024ce513",
+  "apiKey": "AIzaSyAb0E8vi8d2ofBM0sUOl-boppiT-2MHhcc",
+  "authDomain": "studio-5379422803-3d01c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "453370954090"
+};
